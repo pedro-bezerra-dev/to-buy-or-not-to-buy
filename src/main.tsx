498b1df75d24977264045c9276d77591970ac566
Pedro Henrique <pyter.dev@gmail.com>
2022-05-17 21:59:40 -0300

    feat: main file updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './styles/theme'
 
+import './styles/globalStyle.scss'
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
